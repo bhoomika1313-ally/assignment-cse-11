8fe1eba22f1692d313b7e0f841454c3be6be7d4b
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-03-13 12:12:41 +0530

    assigments for mt1

---
+++ a1.js
@@ -0,0 +1,31 @@
+function calculate(){
+    const n = document.getElementById("subjects").value;
+    let totalMarks = 0;
+    let i;
+    for(i = 0; i < n; i++){
+        let x = parseFloat(prompt("Enter the subject number " + (i+1)));
+        totalMarks += x; // can be use with i = 1 too
+    }
+    let average = totalMarks / n;
+    let grade;
+    if(average>90){
+        grade = "A";
+    } else if(average>80){
+        grade = "B";
+    } else if(average>70){
+        grade = "C";
+    } else if(average>60){
+        grade = "D";
+    } else{
+        grade = "F";
+    }
+    let result;
+    if(average>40){
+        result = "Pass";
+    } else{
+        result = "Fail";
+    }
+    let rr = document.getElementById("result").innerHTML = "your total marks are :" 
+    + totalMarks + "<br>" +"Your average marks are: " + average + "<br>" + 
+    "Your grade is: " +  grade + "<br>" + "You have: " + result;
+}
