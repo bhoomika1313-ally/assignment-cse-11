4eb7b8d65e90b415f13127afa2d765b583d1e5f3
bhoomika1313-ally <bhoomikaa1313@gmail.com>
2026-04-06 16:08:38 +0530

    practical

--- a3.js
+++ a3.js
@@ -1,18 +1,28 @@
-let heading = document.getElementById("changetext");
-let paragraph = document.createElement("p");
-let input = document.createElement("input");
-let fontsize = 16;
+let input = document.getElementById("intext");
+let heading = document.getElementById("head");
+let para = document.getElementById("para");
 document.getElementById("changetext").addEventListener("click", function(){
-    
-}
-
-
-
-    // rrggbb = 24 bits 
-    // 2 to the power of 24 = 16777216 
-    // 16777216 - 1 = 16777215
-
-document.getElementById("bgcolor").onclick = function() {
-    
-}
-
+    if(input.value !== ""){
+        heading.innerHTML = input.value;
+    }   });
+document.getElementById("changebg").addEventListener("click", function(){
+    document.body.style.backgroundColor = "lightblue";
+});
+let f = 16;
+document.getElementById("changefont").addEventListener("click", function(){
+    f += 2;
+    heading.style.fontSize = f + "px";
+});
+document.getElementById("hide").addEventListener("click", function(){
+    if(para.style.display === "none"){
+        para.style.display = "block";   
+    } else {
+        para.style.display = "none";
+    }   });
+document.getElementById("reset").addEventListener("click", function(){
+    heading.innerHTML = "welcome to java script lab";
+    document.body.style.backgroundColor = "white";
+    heading.style.fontSize = f;
+    para.style.display = "block";
+    input.value = "";
+});
